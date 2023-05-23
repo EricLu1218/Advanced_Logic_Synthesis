3b30f1578b7b716d5f700147f9beaa33265583e1
Chen-Han Lu <love0416much@gmail.com>
2023-05-23 12:57:15 +0800

    update timer

--- Technology_Mapping/src/main.cpp
+++ Technology_Mapping/src/main.cpp
@@ -1,6 +1,6 @@
 #include "Decomposer/Decomposer.hpp"
 #include "FlowMap/FlowMap.hpp"
-#include "GlobalTimer/GlobalTimer.hpp"
+#include "Timer/Timer.hpp"
 #include "Parser/ArgumentParser.hpp"
 #include "Parser/Parser.hpp"
 
@@ -10,37 +10,37 @@ int main(int argc, char *argv[])
     if (!argParser.parse(argc, argv))
         return 1;
 
-    GlobalTimer globalTimer(10 * 60 - 5);
-    globalTimer.startTimer("runtime");
-    globalTimer.startTimer("parse input");
+    Timer timer(10 * 60 - 5);
+    timer.startTimer("runtime");
+    timer.startTimer("parse input");
 
     Parser parser;
     parser.readBlif(argParser.inputFile);
     auto graph = parser.getGraph();
 
-    globalTimer.stopTimer("parse input");
-    globalTimer.startTimer("decompose");
+    timer.stopTimer("parse input");
+    timer.startTimer("decompose");
 
     Decomposer::decompose(graph.get());
 
-    globalTimer.stopTimer("decompose");
-    globalTimer.startTimer("flow map");
+    timer.stopTimer("decompose");
+    timer.startTimer("flow map");
 
     FlowMap flowMap(graph.get(), argParser.maxLutInputSize);
     auto result = flowMap.solve();
 
-    globalTimer.stopTimer("flow map");
-    globalTimer.startTimer("write output");
+    timer.stopTimer("flow map");
+    timer.startTimer("write output");
 
     result->write(argParser.outputFile);
 
-    globalTimer.stopTimer("write output");
-    globalTimer.stopTimer("runtime");
+    timer.stopTimer("write output");
+    timer.stopTimer("runtime");
 
-    globalTimer.printTime("parse input");
-    globalTimer.printTime("decompose");
-    globalTimer.printTime("flow map");
-    globalTimer.printTime("write output");
-    globalTimer.printTime("runtime");
+    timer.printTime("parse input");
+    timer.printTime("decompose");
+    timer.printTime("flow map");
+    timer.printTime("write output");
+    timer.printTime("runtime");
     return 0;
 }
